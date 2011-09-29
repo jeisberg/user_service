f3d6d92b1ffd37d3a21a50bac833bfab67c7f640
Srijith Sreedharan <srijith@LKRZ-68.local>
2011-09-29 16:06:28 -0700

    Added c3po configs for authentication database

--- src/main/java/com/lockerz/service/user/auth/Authenticator.java
+++ src/main/java/com/lockerz/service/user/auth/Authenticator.java
@@ -2,5 +2,5 @@ package com.lockerz.service.user.auth;
 
 public interface Authenticator {
 	
-	public long authenticate(String token) throws AuthenticatorException;
+	public long authenticate(String apiKey, String token) throws AuthenticatorException;
 }
