779e9202b820d9e74d1f8f16049abc4a689bbfd2
John Eisberg <johneisberg@gmail.com>
2011-09-29 18:14:25 -0700

    Testing

--- src/main/java/com/lockerz/service/user/controllers/UserServiceControllerImpl.java
+++ src/main/java/com/lockerz/service/user/controllers/UserServiceControllerImpl.java
@@ -5,6 +5,9 @@ import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+
+import com.lockerz.service.user.auth.Principal;
+import com.lockerz.service.user.auth.ServicePrincipal;
 import com.lockerz.service.user.models.UserModelImpl;
 import com.lockerz.service.user.services.ServiceImpl;
 import com.lockerz.service.user.utilities.RestException;
@@ -52,14 +55,20 @@ public class UserServiceControllerImpl extends ServiceControllerImpl {
 	@RequestMapping(value="/test", method = RequestMethod.GET)
     public ResponseEntity<UserModelImpl> test(@Principal ServicePrincipal user) 
     throws RestException {
-		// try
+		
+		/*// try
 		try {
-			if(true)
+			if(1 == 1)
 				throw new RestException(user.getName(), null, null);
+			return null;
 		// catch here
 		} catch(ServiceException e) {
 			// throw a rest exception here
 			throw new RestException(e.getMessage(), e.getMessages(), e.getHttpStatus());
-		}
+		}*/
+		
+		if(1 == 1)
+			throw new RestException(user.getName(), null, null);
+		return null;
     } 
 }
